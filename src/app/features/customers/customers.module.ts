ae9c23ca8a3e61f17650dcc4bd21a36cea9635ca
AgusCarrizo23 <agustina.carrizo2012@gmail.com>
2026-07-03 08:26:34 +0200

    feat: se cran componentes, vista y formulario con validaciones para la cracion de clientes

--- src/app/features/customers/customers.module.ts
+++ src/app/features/customers/customers.module.ts
@@ -1,22 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CustomerFormPageComponent } from './pages/customer-form-page/customer-form-page.component';
 import { CustomersPageComponent } from './pages/customers-page/customers-page.component';
 import { CustomerBirthDatePipe } from './pipes/customer-birth-date.pipe';
 import { createCustomerPaginatorIntl } from './utils/customer-paginator-intl';
 
 const routes: Routes = [
+  {
+    path: 'new',
+    component: CustomerFormPageComponent
+  },
   {
     path: '',
     component: CustomersPageComponent
@@ -24,22 +33,31 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [CustomersPageComponent, CustomerBirthDatePipe],
+  declarations: [
+    CustomersPageComponent,
+    CustomerFormPageComponent,
+    CustomerBirthDatePipe
+  ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes),
     MatButtonModule,
     MatCardModule,
+    MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatNativeDateModule,
     MatPaginatorModule,
     MatProgressSpinnerModule,
     MatSelectModule,
+    MatSnackBarModule,
     MatTableModule,
     MatTooltipModule
   ],
   providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'es-AR' },
     {
       provide: MatPaginatorIntl,
       useFactory: createCustomerPaginatorIntl
